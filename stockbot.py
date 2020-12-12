de3d3f70515a45e3c3e396fe06f21f3f87b2e5da
Ryan Cullen <ryantcullen5@gmail.com>
2020-12-11 23:36:29 -0800

    crude bull-flag detection algorithm

--- stockbot.py
+++ stockbot.py
@@ -1,43 +1,110 @@
 import yfinance as yf
 import pandas as _pd
 import numpy as np
-import pylab as pl
+import matplotlib.pyplot as plt
 
+
+def make_functions(i, price, channel):
+    
+    resistance = []
+    support = []
+    xvals = np.linspace(i, i + channel - 1, channel)
+
+    #THESE NEED TO BE ADJUSTED ACCORDING TO STOCK
+    offset = (price/100)/20
+    floor = (price/100)
+    slope = 0.02
+
+    for n in range(channel):
+        resistance.append(price - n*slope + offset)
+
+    for n in range(channel):
+        support.append(price - n*slope - floor + offset)
+
+    return resistance, support, xvals
 
 while True:
     ticker = input("Enter ticker: ")
     if ticker == 'exit':
         break
-    perc = float(input("Enter percentage: "))
+    perc = float(input("Enter prior uptrend percentage: "))
     day_range = int(input("Enter number of trading days: "))
-    trading_start = input("Enter start date (y-m-d): ")
-    trading_end = input("Enter start date (y-m-d): ")
+    channel_range = int(input("Enter channel range: "))
+    # trading_start = input("Enter start date (y-m-d): ")
+    # trading_end = input("Enter start date (y-m-d): ")
+
+    flagpole = False
+    counter = 0
+    makeFuncs = False
+    resistance = []
+    support = []
+    xvals = []
 
     ticker_info = yf.Ticker(ticker)
-    price_history = ticker_info.history(start=trading_start,  end=trading_end)
+    price_history = ticker_info.history(start="2015-9-5",  end="2020-12-11")
 
     opens = price_history['Open']
     closes = price_history['Close']
-    old_price = (opens[0] + closes[0])/2
+    days = opens.size
     prices = []
 
-    days = opens.size
+    fig = plt.figure()
+    
     for i in range(days):
+        
         price = (opens[i] + closes[i])/2
         prices.append(price)
+        
 
-        if (i % day_range) == 0:
-            difference = price - old_price
-            percent_change = (difference/old_price)*100
-            if percent_change > perc:
+        if flagpole:
+            if makeFuncs:
+                resistance, support, xvals = make_functions(i, price, channel_range)
+                makeFuncs = False
+            
+
+            if price > resistance[counter]:
+                if counter > (channel_range/2):
+                    flagpole = False
+                    counter = channel_range
+                else:
+                    flagpole = False
+                    counter = 0
+            elif price < support[counter]:
+                flagpole = False
+                counter = 0
+            else: counter += 1
+                
+
+
+            if counter > channel_range - 1:
+                flagpole = False
+                counter = 0
+                plt.plot(xvals, resistance)
+                plt.plot(xvals, support)
+                print("Found Flag")
                 print(i)
-            old_price = price
+        
+        if i < day_range:
+            old_price = prices[0]
+        else:
+            old_price = prices[i - day_range]
+        
+        difference = price - old_price
+        percent_change = (difference/old_price)*100
+        
+        if percent_change > perc:
+            if flagpole != True:
+                flagpole = True
+                makeFuncs = True
+
 
     x = np.linspace(0, days, days)
     y = prices
-    pl.plot(x,y)
+    plt.plot(x,y)
 
-    pl.show()
+    plt.show()
+
+
     
 
 
