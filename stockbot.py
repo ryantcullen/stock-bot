1a4951e33b6a32f136e34af76e03f5dd0665650f
Ryan Cullen <ryantcullen5@gmail.com>
2020-12-13 17:25:36 -0800

    improve bull flag detection

--- stockbot.py
+++ stockbot.py
@@ -4,21 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def make_functions(i, price, channel):
+def make_functions(i, price, length, slope):
     
     resistance = []
     support = []
-    xvals = np.linspace(i, i + channel - 1, channel)
+    xvals = np.linspace(i, i + length - 1, length)
 
     #THESE NEED TO BE ADJUSTED ACCORDING TO STOCK TO IMPROVE ALGORITHM
-    offset = (price/100)/20
-    floor = (price/100)
-    slope = 0.02
+    offset = (price/100)/10
+    floor = (price/100) * 1.2
 
-    for n in range(channel):
+    for n in range(length):
         resistance.append(price - n*slope + offset)
 
-    for n in range(channel):
+    for n in range(length):
         support.append(price - n*slope - floor + offset)
 
     return resistance, support, xvals
@@ -30,15 +29,11 @@ while True:
     perc = float(input("Enter prior uptrend percentage: "))
     day_range = int(input("Enter number of trading days: "))
     channel_range = int(input("Enter channel range: "))
-    # trading_start = input("Enter start date (y-m-d): ")
-    # trading_end = input("Enter start date (y-m-d): ")
+
 
     flagpole = False
     counter = 0
     makeFuncs = False
-    resistance = []
-    support = []
-    xvals = []
 
     ticker_info = yf.Ticker(ticker)
     price_history = ticker_info.history(start="2015-9-5",  end="2020-12-11")
@@ -55,28 +50,30 @@ while True:
         
         price = (opens[i] + closes[i])/2
         prices.append(price)
+        slope = closes[days-1]/5000
         
 
         if flagpole:
             if makeFuncs:
-                resistance, support, xvals = make_functions(i, price, channel_range)
+                resistance, support, xvals = make_functions(i, price, channel_range, slope)
                 makeFuncs = False
             
 
             if price > resistance[counter]:
-                if counter > (channel_range/2):
+                if counter > (channel_range/5):
                     flagpole = False
                     counter = channel_range
                 else:
                     flagpole = False
                     counter = 0
             elif price < support[counter]:
-                flagpole = False
-                counter = 0
+                if counter > (channel_range/5):
+                    flagpole = False
+                    counter = 0
+                else:
+                    counter += 1
             else: counter += 1
                 
-                
-
 
             if counter > channel_range - 1:
                 flagpole = False
@@ -87,14 +84,18 @@ while True:
                 print(i)
         
         if i < day_range:
-            old_price = prices[0]
+            initial_price = prices[0]
+            pole_price = prices[0]
         else:
-            old_price = prices[i - day_range]
+            initial_price = prices[i - day_range]
+            pole_price = prices[i - int(day_range/10)]
         
-        difference = price - old_price
-        percent_change = (difference/old_price)*100
+        runup_difference = price - initial_price
+        pole_difference = price - pole_price
+        runup = (runup_difference/initial_price)*100
+        pole = (pole_difference/pole_price)*100
         
-        if percent_change > perc:
+        if (runup > perc) & (pole > (perc/2)):
             if flagpole != True:
                 flagpole = True
                 makeFuncs = True
